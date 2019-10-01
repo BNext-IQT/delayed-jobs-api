0b5e95e5f8635a51f0712660380138fc516deaa0
David Mendez <dmendez@ebi.ac.uk>
2019-10-01 16:11:24 +0100

    try connection with LSF

--- test_lsf_connection.sh
+++ test_lsf_connection.sh
@@ -2,7 +2,7 @@
 
 IDENTITY_FILE=${1:-'~/.ssh/id_rsa'}
 
-ssh cbl_sw@noah-login -i $IDENTITY_FILE -v <<ENDSSH
+ssh cbl_sw@noah-login -i $IDENTITY_FILE -v -oStrictHostKeyChecking=no <<ENDSSH
 echo '------'
 whoami
 bjobs
