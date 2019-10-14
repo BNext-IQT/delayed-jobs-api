22c0d984ccdd555d38a999726c7f1c5cf5434d6e
David Mendez <dmendez@ebi.ac.uk>
2019-10-14 12:03:27 +0100

    DelayedJob: add basic properties

--- app/models/delayed_job_models.py
+++ app/models/delayed_job_models.py
@@ -6,6 +6,7 @@ import hashlib
 import base64
 from enum import Enum
 from app.models.db import db
+import datetime
 
 
 class JobTypes(Enum):
@@ -22,11 +23,35 @@ class JobTypes(Enum):
         return self.name
 
 
-class DelayedJob(db.Model):
-    id = db.Column(db.String, primary_key=True)
+class JobStatuses(Enum):
+    """
+        Possible statuses of delayed jobs
+    """
+    CREATED = 'CREATED'
+    QUEUED = 'QUEUED'
+    RUNNING = 'RUNNING'
+    ERROR = 'ERROR'
+    FINISHED = 'FINISHED'
 
     def __repr__(self):
-        return '<DelayedJob %r>' % self.id
+        return self.name
+
+
+class DelayedJob(db.Model):
+    id = db.Column(db.String(length=60), primary_key=True)
+    type = db.Column(db.Enum(JobTypes))
+    status = db.Column(db.Enum(JobStatuses), default=JobStatuses.CREATED)
+    status_comment = db.Column(db.String) # a comment about the status, for example 'Compressing file'
+    progress = db.Column(db.Integer, default=0)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    queued_at = db.Column(db.DateTime)
+    started_at = db.Column(db.DateTime)
+    finished_at = db.Column(db.DateTime)
+    output_file_path = db.Column(db.Text)
+    log = db.Column(db.Text)
+
+    def __repr__(self):
+        return f'<DelayedJob ${self.id} ${self.type} ${self.status}>'
 
 
 def generate_job_id(job_type, job_params):
@@ -51,7 +76,7 @@ def get_or_create(job_type, job_params):
     print('Getting or creating job!!!')
     id = generate_job_id(job_type, job_params)
     print('id: ', id)
-    job = DelayedJob(id=id)
+    job = DelayedJob(id=id, type=job_type)
     print('job: ', job)
     db.session.add(job)
     db.session.commit()
--- app/models/test/test_models.py
+++ app/models/test/test_models.py
@@ -2,12 +2,13 @@
 Tests for the delayed job model
 """
 import unittest
-import app.models.delayed_job_models as DelayedJob
+from app.models import delayed_job_models
 import json
 import hashlib
 import base64
 from app.models.db import db
 from app import create_app
+import datetime
 
 flask_app = create_app()
 db.init_app(flask_app)
@@ -20,12 +21,12 @@ class MyTestCase(unittest.TestCase):
             db.create_all()
 
     def test_job_id_is_generated_correctly(self):
-        job_type = DelayedJob.JobTypes.SIMILARITY
+        job_type = delayed_job_models.JobTypes.SIMILARITY
         params = {
             'structure': '[H]C1(CCCN1C(=N)N)CC1=NC(=NO1)C1C=CC(=CC=1)NC1=NC(=CS1)C1C=CC(Br)=CC=1',
             'threshold': '70'
         }
-        id_got = DelayedJob.generate_job_id(job_type, params)
+        id_got = delayed_job_models.generate_job_id(job_type, params)
 
         stable_raw_search_params = json.dumps(params, sort_keys=True)
         search_params_digest = hashlib.sha256(stable_raw_search_params.encode('utf-8')).digest()
@@ -38,15 +39,25 @@ class MyTestCase(unittest.TestCase):
     def test_a_job_is_created(self):
 
         with flask_app.app_context():
-            job_type = DelayedJob.JobTypes.SIMILARITY
+            job_type = delayed_job_models.JobTypes.SIMILARITY
             params = {
                 'structure': '[H]C1(CCCN1C(=N)N)CC1=NC(=NO1)C1C=CC(=CC=1)NC1=NC(=CS1)C1C=CC(Br)=CC=1',
                 'threshold': '70'
             }
 
-            job_must_be = DelayedJob.get_or_create(job_type, params)
+            job_must_be = delayed_job_models.get_or_create(job_type, params)
             job_id_must_be = job_must_be.id
             print('job_id_must_be: ', job_id_must_be)
+            job_got = delayed_job_models.DelayedJob.query.filter_by(id=job_id_must_be).first()
+            self.assertEqual(job_type, job_got.type, msg='The job type was not saved correctly.')
+            self.assertEqual(delayed_job_models.JobStatuses.CREATED, job_got.status,
+                             msg='The job status was not saved correctly.')
+            self.assertEqual(0, job_got.progress, msg='The job progress was not saved correctly.')
+
+            created_at_got = job_got.created_at.timestamp()
+            created_at_must_be = datetime.datetime.utcnow().timestamp()
+            self.assertAlmostEqual(created_at_got, created_at_must_be, places=1,
+                                   msg='The created time was not calculated correctly')
 
 
 if __name__ == '__main__':
