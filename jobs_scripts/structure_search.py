fa380c31ca9d357ea4c116aedfb7261daaeac852
David Mendez <dmendez@ebi.ac.uk>
2019-10-24 12:28:53 +0100

    Start first basic version of structure search script

---
+++ jobs_scripts/structure_search.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('run_params_file', help='The path of the file with the run params')
+parser.add_argument('-v', '--verbose', help='Make output verbose', action="store_true")
+args = parser.parse_args()
+
+
+def run(run_params_file, be_verbose):
+    print('RUNNING...')
+    print('run_params_file: ', run_params_file)
+    print('be_verbose: ', be_verbose)
+
+
+if __name__ == "__main__":
+    run(args.run_params_file, args.verbose)
