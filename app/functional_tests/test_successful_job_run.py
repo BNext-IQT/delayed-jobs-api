a9096a2ca1687dff69652066ad63cd48bc566581
David Mendez <dmendez@ebi.ac.uk>
2019-10-28 10:12:19 +0000

    Finnish first functional test for successful job

--- app/functional_tests/test_successful_job_run.py
+++ app/functional_tests/test_successful_job_run.py
@@ -1,5 +1,6 @@
 import requests
 import time
+import os
 
 
 def run_test(server_base_url):
@@ -27,6 +28,28 @@ def run_test(server_base_url):
     status_response = status_request.json()
 
     job_status = status_response.get('status')
-    print('job_status: ', job_status)
-    print('status_response code: ', status_response)
+    print(f'job_status: {job_status}')
+    assert job_status == 'RUNNING', 'Job seems to not be running!'
+
+    progress = int(status_response.get('progress'))
+    print(f'progress:  {progress}')
+    assert progress > 0, 'The job progress is not increasing'
+
+    print('wait some time until it should have finished...')
+    time.sleep((seconds / 2) + 1)
+
+    status_request = requests.get(f'{server_base_url}/status/{job_id}')
+    status_response = status_request.json()
+
+    job_status = status_response.get('status')
+    print(f'job_status: {job_status}')
+    assert job_status == 'FINISHED', 'Job should have finished already!'
+
+    output_file_path = status_response.get('output_file_path')
+    print(f'output_file_path: {output_file_path}')
+    assert os.path.isfile(output_file_path), 'The output file was not created!'
+
+
+
+
 
