1bc495b1c107d9a62390f87afe6d737b74727d27
David Mendez <dmendez@ebi.ac.uk>
2020-01-31 14:52:44 +0000

    Allow to configure the url of the server for feedback from job

--- app/namespaces/job_submission/services/job_submission_service.py
+++ app/namespaces/job_submission/services/job_submission_service.py
@@ -237,7 +237,8 @@ def create_params_file(job, input_files_desc):
         'inputs': prepare_job_inputs(job, input_files_desc),
         'output_dir': get_job_output_dir_path(job),
         'status_update_endpoint': {
-            'url': f'http://127.0.0.1:5000{RUN_CONFIG.get("base_path", "")}/status/{job.id}',
+            'url': f'{RUN_CONFIG.get("job_feedback_base_url", "http://127.0.0.1:5000")}'
+                   f'{RUN_CONFIG.get("base_path", "")}/status/{job.id}',
             'method': 'PATCH'
         },
         'job_params': json.loads(job.raw_params)
