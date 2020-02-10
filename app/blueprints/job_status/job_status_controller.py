dc342758c639d83c6e1448e9743450e0978bcdf6
David Mendez <dmendez@ebi.ac.uk>
2020-02-10 15:31:08 +0000

    Set up job api key auth in swagger description

--- app/blueprints/job_status/job_status_controller.py
+++ app/blueprints/job_status/job_status_controller.py
@@ -1,14 +1,24 @@
 """
 The blueprint used for handling the jobs status
 """
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, jsonify, abort
 
 from app.blueprints.job_status import job_status_service
+from app.authorisation.decorators import token_required_for_job_id
 
 JOB_STATUS_BLUEPRINT = Blueprint('job_status', __name__)
 
-@JOB_STATUS_BLUEPRINT.route('/<job_id>')
+@JOB_STATUS_BLUEPRINT.route('/<job_id>', methods = ['GET'])
 def get_job_status(job_id):
 
-    response = job_status_service.get_job_status(job_id)
-    return jsonify(response)
+    try:
+        return jsonify(job_status_service.get_job_status(job_id))
+    except job_status_service.JobNotFoundError:
+        abort(404)
+
+@JOB_STATUS_BLUEPRINT.route('/<job_id>', methods = ['PATCH'])
+@token_required_for_job_id
+def update_job_progress(job_id):
+
+    print('UPDATE JOB PROGRESS!')
+    return jsonify({})
