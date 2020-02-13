0bbdf69a338761a97b820fd2f64ec51bc83fc464
David Mendez <dmendez@ebi.ac.uk>
2020-02-13 14:28:50 +0000

    update docker image for test job

--- app/blueprints/job_submission/controllers/job_submissions_controller.py
+++ app/blueprints/job_submission/controllers/job_submissions_controller.py
@@ -12,7 +12,7 @@ SUBMISSION_BLUEPRINT = Blueprint('job_submission', __name__)
 def submit_test_job():
 
     job_type = delayed_job_models.JobTypes.TEST
-    docker_image_url = 'dockerhub.ebi.ac.uk/chembl/chembl/delayed-jobs/test-job:cfc42a35-final'
+    docker_image_url = 'docker://dockerhub.ebi.ac.uk/chembl/chembl/delayed-jobs/test-job:cfc42a35-final'
 
     response = job_submission_service.parse_args_and_submit_job(job_type, request.form, request.files, docker_image_url)
     return jsonify(response)
