dacbc08c86389abd2b8634a1e0af91b8c3161e70
David Mendez <dmendez@ebi.ac.uk>
2020-02-17 15:39:00 +0000

    Write to logs the return code of the submission

--- app/job_status_daemon/test/test_status_daemon.py
+++ app/job_status_daemon/test/test_status_daemon.py
@@ -14,6 +14,17 @@ class TestJobStatusDaemon(unittest.TestCase):
         self.flask_app = create_app()
         self.client = self.flask_app.test_client()
 
+    def tearDown(self):
+        print('TEAR DOWN')
+
+    def test_determines_for_which_jobs_check_status_0(self):
+        """
+        Given a set of jobs currently in the database, knows for which it is required to check the status.
+        In this case, some jobs require a check.
+        """
+        print('TEST FOR WHICH JOB CHECK JOB STATUS')
+
+
     def test_mapping_of_lsf_job_status(self):
 
         print('TEST MAPPING OF JOB STATUS')
