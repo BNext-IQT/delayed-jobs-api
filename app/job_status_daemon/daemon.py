cd5ccb376ba9d1a17200d0c47bcdf24336f6a378
David Mendez <dmendez@ebi.ac.uk>
2020-02-18 10:27:54 +0000

    Status Agent: Return a list of jobs for which to check the status

--- app/job_status_daemon/daemon.py
+++ app/job_status_daemon/daemon.py
@@ -1,3 +1,30 @@
 """
 Module that implements the daemon that checks the statuses of jobs in LSF
 """
+from sqlalchemy import and_
+
+from app.models import delayed_job_models
+from app.config import RUN_CONFIG
+
+
+def get_lsf_job_ids_to_check():
+    """
+    :return: a list of LSF job IDs for which it is necessary check the status in the LSF cluster. The jobs that are
+    checked are the ones that:
+    1. Were submitted to the same LSF cluster that I am running with (defined in configuration)
+    2. Are not in Error or Finished state.
+    """
+
+    status_is_not_error_or_finished = delayed_job_models.DelayedJob.status.notin_(
+        [delayed_job_models.JobStatuses.ERROR, delayed_job_models.JobStatuses.FINISHED]
+    )
+
+    lsf_config = RUN_CONFIG.get('lsf_submission')
+    lsf_host = lsf_config['lsf_host']
+    lsf_host_is_my_host = delayed_job_models.DelayedJob.lsf_host == lsf_host
+
+    job_to_check_status = delayed_job_models.DelayedJob.query.filter(
+        and_(lsf_host_is_my_host, status_is_not_error_or_finished)
+    )
+
+    return [job.lsf_job_id for job in job_to_check_status]
--- app/job_status_daemon/test/test_status_daemon.py
+++ app/job_status_daemon/test/test_status_daemon.py
@@ -3,9 +3,12 @@ This Module tests the basic functions of the status daemon
 """
 import unittest
 
+from sqlalchemy import and_
+
 from app import create_app
 from app.models import delayed_job_models
 from app.config import RUN_CONFIG
+from app.job_status_daemon import daemon
 
 
 class TestJobStatusDaemon(unittest.TestCase):
@@ -41,26 +44,40 @@ class TestJobStatusDaemon(unittest.TestCase):
                 lsf_host = lsf_config['lsf_host']
 
                 for assigned_host in [lsf_host, 'another_host']:
-
                     job = delayed_job_models.DelayedJob(
                         id=f'Job-{assigned_host}-{status}',
                         type='TEST',
                         lsf_job_id=i,
-                        status=delayed_job_models.JobStatuses.CREATED,
+                        status=status,
                         lsf_host=assigned_host
                     )
                     delayed_job_models.save_job(job)
                     i += 1
-
 
     def test_determines_for_which_jobs_check_status_0(self):
         """
         Given a set of jobs currently in the database, knows for which it is required to check the status.
         In this case, some jobs require a check.
         """
-        print('TEST FOR WHICH JOB CHECK JOB STATUS')
         self.create_test_jobs_0()
 
+        with self.flask_app.app_context():
+            lsf_config = RUN_CONFIG.get('lsf_submission')
+            lsf_host = lsf_config['lsf_host']
+
+            status_is_not_error_or_finished = delayed_job_models.DelayedJob.status.notin_(
+                [delayed_job_models.JobStatuses.ERROR, delayed_job_models.JobStatuses.FINISHED]
+            )
+            lsf_host_is_my_host = delayed_job_models.DelayedJob.lsf_host == lsf_host
+
+            job_to_check_status_must_be = delayed_job_models.DelayedJob.query.filter(
+                and_(lsf_host_is_my_host, status_is_not_error_or_finished)
+            )
+
+            lsf_ids_to_check_status_must_be = [job.lsf_job_id for job in job_to_check_status_must_be]
+            lsf_ids_to_check_got = daemon.get_lsf_job_ids_to_check()
+            self.assertListEqual(lsf_ids_to_check_status_must_be, lsf_ids_to_check_got,
+                                 msg='The jobs for which to check the status were not created correctly!')
 
     def test_mapping_of_lsf_job_status(self):
 
