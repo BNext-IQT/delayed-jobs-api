d8a040becba4fc0f114c5cc2b02cda2ffe9a6ea8
David Mendez <dmendez@ebi.ac.uk>
2020-02-19 16:19:16 +0000

    Job Status Agent: react when job switches state to running

--- app/job_status_daemon/test/test_status_daemon.py
+++ app/job_status_daemon/test/test_status_daemon.py
@@ -213,3 +213,21 @@ class TestJobStatusDaemon(unittest.TestCase):
             status_got = job.status
             status_must_be = delayed_job_models.JobStatuses.RUNNING
             self.assertEqual(status_got, status_must_be, msg='The status of the job was not changed accordingly!')
+
+    def test_parses_the_output_of_bjobs_pending_job(self):
+        """
+        Generates mock jobs, then sends a mock output to the the function to test that it interpreted the output
+        accordingly. This test focuses on a job that switched to pending state.
+        """
+        self.create_test_jobs_0()
+
+        sample_output = self.load_sample_file('app/job_status_daemon/test/data/sample_lsf_output_2.txt')
+
+        with self.flask_app.app_context():
+            daemon.parse_bjobs_output(sample_output)
+            # job with lsf id 0 should be in running state now
+            lsf_job_id = 0
+            job = delayed_job_models.get_job_by_lsf_id(lsf_job_id)
+            status_got = job.status
+            status_must_be = delayed_job_models.JobStatuses.QUEUED
+            self.assertEqual(status_got, status_must_be, msg='The status of the job was not changed accordingly!')
