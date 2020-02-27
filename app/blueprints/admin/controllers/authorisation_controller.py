634e9cf6869b77d26aa9dd733739ef5856b72cd1
David Mendez <dmendez@ebi.ac.uk>
2020-02-27 14:13:00 +0000

    Implement admin authorisation service

--- app/blueprints/admin/controllers/authorisation_controller.py
+++ app/blueprints/admin/controllers/authorisation_controller.py
@@ -1,11 +1,19 @@
 """
 Blueprint for authorisation endpoints for the administration of the system
 """
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request, make_response
+
+from app.blueprints.admin.services import authorisation_service
 
 ADMIN_AUTH_BLUEPRINT = Blueprint('admin_auth', __name__)
 
 @ADMIN_AUTH_BLUEPRINT.route('/login', methods = ['GET'])
 def login():
-    print('LOGGING IN!')
-    return jsonify({'token': 'kkk'})
+
+    auth = request.authorization
+
+    try:
+        token = authorisation_service.get_admin_token(auth.username, auth.password)
+        return jsonify({'token': token})
+    except authorisation_service.InvalidCredentialsError as error:
+        return make_response(str(error), 401, {'WWW-Authenticate': 'Basic realm="Login Required'})
