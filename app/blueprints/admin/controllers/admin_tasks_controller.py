c054141aeaa3248a421f942119b732146308a16f
David Mendez <dmendez@ebi.ac.uk>
2020-02-27 16:45:05 +0000

    Require admin token for job deletion by type

--- app/blueprints/admin/controllers/admin_tasks_controller.py
+++ app/blueprints/admin/controllers/admin_tasks_controller.py
@@ -4,9 +4,12 @@ Blueprint for the administrative tasks of the system
 from flask import Blueprint, jsonify, request, make_response
 
 from app.blueprints.admin.services import admin_tasks_service
+from app.authorisation.decorators import admin_token_required
+
 ADMIN_TASKS_BLUEPRINT = Blueprint('admin_tasks', __name__)
 
 @ADMIN_TASKS_BLUEPRINT.route('/delete_all_jobs_by_type', methods = ['POST'])
+@admin_token_required
 def delete_all_jobs_by_type():
 
     form_data = request.form
