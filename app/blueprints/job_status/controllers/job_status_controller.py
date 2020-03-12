dbba73e9901a47cfbcb7aabdd0c786ff0e4b6d2b
David Mendez <dmendez@ebi.ac.uk>
2020-03-12 15:31:47 +0000

    Add Marshmallow schemas por job update and status endpoints

--- app/blueprints/job_status/controllers/job_status_controller.py
+++ app/blueprints/job_status/controllers/job_status_controller.py
@@ -5,10 +5,13 @@ from flask import Blueprint, jsonify, abort, request
 
 from app.authorisation.decorators import token_required_for_job_id
 from app.blueprints.job_status.services import job_status_service
+from app.blueprints.job_status.controllers import marshmallow_schemas
+from app.request_validation.decorators import validate_form_with, validate_url_params_with
 
 JOB_STATUS_BLUEPRINT = Blueprint('job_status', __name__)
 
 @JOB_STATUS_BLUEPRINT.route('/<job_id>', methods = ['GET'])
+@validate_url_params_with(marshmallow_schemas.JobStatus)
 def get_job_status(job_id):
 
     try:
@@ -18,6 +21,8 @@ def get_job_status(job_id):
 
 @JOB_STATUS_BLUEPRINT.route('/<job_id>', methods = ['PATCH'])
 @token_required_for_job_id
+@validate_url_params_with(marshmallow_schemas.JobStatus)
+@validate_form_with(marshmallow_schemas.JobStatusUpdate)
 def update_job_progress(job_id):
 
     try:
---
+++ app/blueprints/job_status/controllers/marshmallow_schemas.py
@@ -0,0 +1,19 @@
+"""
+Schemas to validate the input of job status Endpoint
+"""
+from marshmallow import Schema, fields, validate
+
+class JobStatus(Schema):
+    """
+    Class that the schema for getting a job status job by id
+    """
+    job_id = fields.String(required=True)
+
+class JobStatusUpdate(Schema):
+    """
+    Class that the schema for updating a job status job by id
+    """
+    progress = fields.Number(required=True, validate=validate.Range(min=0, max=100))
+    status_log = fields.String()
+    status_description = fields.String()
+
