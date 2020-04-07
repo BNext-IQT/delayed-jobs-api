ee562ad9a72362e18651c1db8e58f68be2631922
David Mendez <dmendez@ebi.ac.uk>
2020-04-07 17:44:21 -0500

    Remove tests using Input File

--- app/models/test/utils.py
+++ app/models/test/utils.py
@@ -5,21 +5,6 @@ import os
 
 from app.models import delayed_job_models
 
-def simulate_inputs_to_job(job, job_run_dir):
-    """
-    Creates some input files and adds it to the job object
-    :param job: job to add the input files to
-    :param job_run_dir: directory where the job runs
-    """
-    inputs_path = os.path.join(job_run_dir, 'inputs')
-    for i in range(1, 5):
-        input_path = os.path.join(inputs_path, f'input{i}.txt')
-        with open(inputs_path, 'w') as input_file:
-            input_file.write(f'This is input {i}')
-
-        job_input_file = delayed_job_models.InputFile(internal_path=input_path)
-        delayed_job_models.add_input_file_to_job(job, job_input_file)
-
 def simulate_outputs_of_job(job, output_dir):
 
     """
