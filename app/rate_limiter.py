4ed0e9ad9155c804a043188dfa7020ed6689daa4
David Mendez <dmendez@ebi.ac.uk>
2020-04-09 09:37:43 -0500

    Set up default limit as 3 per second

--- app/rate_limiter.py
+++ app/rate_limiter.py
@@ -4,9 +4,7 @@ Module that defines the instance of the rate limiter
 from flask_limiter import Limiter
 from flask_limiter.util import get_ipaddr
 
-PER_SECOND = 0.5
-
 RATE_LIMITER = Limiter(
     key_func=get_ipaddr,
-    default_limits=['1 per day']
+    default_limits=['3 per second']
 )
