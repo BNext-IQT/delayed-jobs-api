8eb8ffe70e8bf4175e8aa5ffe41f3370cad0296b
David Mendez <dmendez@ebi.ac.uk>
2020-04-21 12:25:29 -0500

    Calculate the time from when a job starts to when it finished

--- app/job_status_daemon/job_statistics/statistics_generator.py
+++ app/job_status_daemon/job_statistics/statistics_generator.py
@@ -3,10 +3,20 @@ This Module generates generic statistics for a job
 """
 def get_seconds_from_created_to_queued(job):
     """
-    :param job: DelayedJob object for which to do the caculation.
+    :param job: DelayedJob object for which to do the calculation.
     :return: the amount of seconds that passed from the time the job was created to the time it
     was queued
     """
     created_at = job.created_at
     started_at = job.started_at
     return (started_at - created_at).total_seconds()
+
+def get_seconds_from_running_to_finished(job):
+    """
+    :param job: DelayedJob object for which to do the calculation.
+    :return: the amount of seconds that passed from the time the job was running to the time it
+    was finished
+    """
+    started_at = job.started_at
+    finished_at = job.finished_at
+    return (finished_at - started_at).total_seconds()
--- app/job_status_daemon/test/test_statistics_generation.py
+++ app/job_status_daemon/test/test_statistics_generation.py
@@ -27,9 +27,9 @@ class TestJobStatisticsGeneration(unittest.TestCase):
             delayed_job_models.delete_all_jobs()
             shutil.rmtree(daemon.AGENT_RUN_DIR, ignore_errors=True)
 
-    def test_generates_calculates_time_from_created_to_queued(self):
+    def test_calculates_time_from_created_to_running(self):
         """
-        tests that generates the statistics for a job that finished normally
+        test that it calculates the time from created to running
         """
 
         current_time = datetime.now()
@@ -47,3 +47,27 @@ class TestJobStatisticsGeneration(unittest.TestCase):
         seconds_got = statistics_generator.get_seconds_from_created_to_queued(job)
         self.assertEqual(seconds_got, seconds_must_be,
                          msg='The seconds from created to queued were not calculated correctly!')
+
+    def test_calculates_time_from_running_to_finished(self):
+        """
+        test that it calculates the time from running to finished
+        """
+
+        current_time = datetime.now()
+        started_at = current_time
+        seconds_must_be = 1.5
+        finished_at = started_at + timedelta(seconds=seconds_must_be)
+
+        job = delayed_job_models.DelayedJob(
+            id=f'Job-Finished',
+            type='TEST',
+            started_at=started_at,
+            finished_at=finished_at
+        )
+
+        seconds_got = statistics_generator.get_seconds_from_running_to_finished(job)
+        self.assertEqual(seconds_got, seconds_must_be,
+                         msg='The seconds from created to queued were not calculated correctly!')
+
+
+
