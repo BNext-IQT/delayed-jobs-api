3c2d03c3beaf2d3ac413cf4c9890081fa4efccbb
David Mendez <dmendez@ebi.ac.uk>
2020-04-22 10:16:06 -0500

    Finish to implement function that generates dict to save job record

--- app/job_statistics/statistics_saver.py
+++ app/job_statistics/statistics_saver.py
@@ -1,17 +1,35 @@
 """
 This Module saves statistics for the jobs in elasticsearch
 """
-def get_job_record_dict(job_type, run_env_type, lsf_host, started_at):
+def get_job_record_dict(job_type, run_env_type, lsf_host, started_at, finished_at,
+                        seconds_taken_from_created_to_running, seconds_taken_from_running_to_finished_or_error,
+                        final_state, num_output_files, total_output_bytes, num_input_files, total_input_bytes):
     """
     :param job_type: type of the job
     :param run_env_type: run environment
     :param lsf_host: lsf host
     :param started_at: started_at timestamp
+    :param finished_at: finished_at timestamp
+    :param seconds_taken_from_created_to_running: seconds_taken_from_created_to_running
+    :param seconds_taken_from_running_to_finished_or_error: seconds_taken_from_running_to_finished_or_error
+    :param final_state: final_state of the job
+    :param num_output_files: number of output files of the job
+    :param total_output_bytes: total output of bytes of the job
+    :param num_input_files: number of input files of the job
+    :param total_input_bytes: total input of bytes of the job
     :return: a dict with a record of a job to be saved in elasticsearch
     """
     return {
         'job_type': job_type,
         'run_env_type': run_env_type,
         'lsf_host': lsf_host,
-        'started_at': started_at
+        'started_at': started_at,
+        'finished_at': finished_at,
+        'seconds_taken_from_created_to_running': seconds_taken_from_created_to_running,
+        'seconds_taken_from_running_to_finished_or_error': seconds_taken_from_running_to_finished_or_error,
+        'final_state': final_state,
+        'num_output_files': num_output_files,
+        'total_output_bytes': total_output_bytes,
+        'num_input_files': num_input_files,
+        'total_input_bytes': total_input_bytes
     }
--- app/job_statistics/test/test_statistics_saver.py
+++ app/job_statistics/test/test_statistics_saver.py
@@ -20,7 +20,15 @@ class TestStatisticsSaver(unittest.TestCase):
             'job_type': 'TEST',
             'run_env_type': 'DEV',
             'lsf_host': 'some_host',
-            'started_at': (current_time + timedelta(seconds=1)).timestamp() * 1000
+            'started_at': (current_time + timedelta(seconds=1)).timestamp() * 1000,
+            'finished_at': (current_time + timedelta(seconds=2)).timestamp() * 1000,
+            'seconds_taken_from_created_to_running': 1,
+            'seconds_taken_from_running_to_finished_or_error': 1,
+            'final_state': 'FINISHED',
+            'num_output_files': 2,
+            'total_output_bytes': 100,
+            'num_input_files': 3,
+            'total_input_bytes': 300
         }
 
         job_record_dict_got = statistics_saver.get_job_record_dict(
@@ -28,6 +36,15 @@ class TestStatisticsSaver(unittest.TestCase):
             run_env_type=dict_must_be.get('run_env_type'),
             lsf_host=dict_must_be.get('lsf_host'),
             started_at=dict_must_be.get('started_at'),
+            finished_at=dict_must_be.get('finished_at'),
+            seconds_taken_from_created_to_running=dict_must_be.get('seconds_taken_from_created_to_running'),
+            seconds_taken_from_running_to_finished_or_error=
+            dict_must_be.get('seconds_taken_from_running_to_finished_or_error'),
+            final_state=dict_must_be.get('final_state'),
+            num_output_files=dict_must_be.get('num_output_files'),
+            total_output_bytes=dict_must_be.get('total_output_bytes'),
+            num_input_files=dict_must_be.get('num_input_files'),
+            total_input_bytes=dict_must_be.get('total_input_bytes')
         )
 
         self.assertEqual(job_record_dict_got, dict_must_be, 'The job record dict was not generated correctly')
