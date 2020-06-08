7907222acbe44f69dc7df5faad327e5a32859f6c
David Mendez <dmendez@ebi.ac.uk>
2020-06-08 14:27:40 -0500

    Add endpoint to submit BLAST Job statistics

--- app/blueprints/custom_statistics/controllers/custom_statistics_controller.py
+++ app/blueprints/custom_statistics/controllers/custom_statistics_controller.py
@@ -10,24 +10,24 @@ from app.blueprints.custom_statistics.services import custom_statistics_service
 
 CUSTOM_STATISTICS_BLUEPRINT = Blueprint('custom_statistics', __name__)
 
-@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/test_job/<job_id>', methods = ['POST'])
+
+@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/test_job/<job_id>', methods=['POST'])
 @token_required_for_job_id
 @validate_url_params_with(marshmallow_schemas.JobID)
 @validate_form_with(marshmallow_schemas.TestJobStatistics)
 def submit_custom_statistics_test_job(job_id):
-
     try:
         duration = request.form.get('duration')
         return jsonify(custom_statistics_service.save_custom_statistics_test_job(job_id, duration))
     except custom_statistics_service.JobNotFoundError:
         abort(404, f'Job with id {job_id} does not exist!')
 
-@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/structure_search_job/<job_id>', methods = ['POST'])
+
+@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/structure_search_job/<job_id>', methods=['POST'])
 @token_required_for_job_id
 @validate_url_params_with(marshmallow_schemas.JobID)
 @validate_form_with(marshmallow_schemas.StructureSearchJobStatistics)
 def submit_custom_statistics_structure_search_job(job_id):
-
     try:
         search_type = request.form.get('search_type')
         time_taken = request.form.get('time_taken')
@@ -36,12 +36,26 @@ def submit_custom_statistics_structure_search_job(job_id):
     except custom_statistics_service.JobNotFoundError:
         abort(404, f'Job with id {job_id} does not exist!')
 
-@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/mmv_job/<job_id>', methods = ['POST'])
+
+@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/biological_sequence_search_job/<job_id>', methods=['POST'])
+@token_required_for_job_id
+@validate_url_params_with(marshmallow_schemas.JobID)
+@validate_form_with(marshmallow_schemas.BiologicalSequenceSearchJobStatistics)
+def submit_custom_statistics_biological_sequence_search_job(job_id):
+    try:
+        search_type = 'BLAST'
+        time_taken = request.form.get('time_taken')
+        return jsonify(custom_statistics_service.save_custom_statistics_structure_search_job(
+            job_id, search_type, time_taken))
+    except custom_statistics_service.JobNotFoundError:
+        abort(404, f'Job with id {job_id} does not exist!')
+
+
+@CUSTOM_STATISTICS_BLUEPRINT.route('/submit_statistics/mmv_job/<job_id>', methods=['POST'])
 @token_required_for_job_id
 @validate_url_params_with(marshmallow_schemas.JobID)
 @validate_form_with(marshmallow_schemas.MMVSearchJobStatistics)
 def submit_custom_statistics_mmv_job(job_id):
-
     try:
         num_sequences = request.form.get('num_sequences')
         return jsonify(custom_statistics_service.save_custom_statistics_mmv_job(job_id, num_sequences))
